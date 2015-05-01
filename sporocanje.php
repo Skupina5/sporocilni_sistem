f84e641dea72870bf7c9568de31fb9b0aad45048
lukakadunc <luka.7.kadunc@gmail.com>
2015-05-01 14:40:54 +0200

    w
    w

--- sporocanje.php
+++ sporocanje.php
@@ -14,11 +14,11 @@ $_SESSION["uporabnik_id"] = $_GET["uporid"];
 
 <body>
 
-    <ul id="meni1">
-      <li id="box1"><a href = "inbox.php">Prejeta sporocila</a></li>
-        <li id="box1"><a href = "sent.php">Poslana sporocila</a></li>
-        <li id="box1"><a href = "new_spor.html">Novo sporocilo</a></li>
-        <li id="box1"><a href = "skupine.html">Skupine</a></li>
+    <ul id="sidemeni">
+      <li id="box2"><a href = "inbox.php">Prejeta sporocila</a></li>
+        <li id="box2"><a href = "sent.php">Poslana sporocila</a></li>
+        <li id="box2"><a href = "new_spor.html">Novo sporocilo</a></li>
+        <li id="box2"><a href = "skupine.html">Skupine</a></li>
         <li id="odjava"><a href="index.html"><img id="slika1" src="odjava.gif"</a></a></li>
 
     </ul>
